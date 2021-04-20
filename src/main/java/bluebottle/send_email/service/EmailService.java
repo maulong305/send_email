925a2ed17ff985b0c91ea907733fc3319fce74bc
MauLong <maulong305@gmail.com>
2021-04-20 12:05:03 +0700

    send email using sdk and rest template

--- src/main/java/bluebottle/send_email/service/EmailService.java
+++ src/main/java/bluebottle/send_email/service/EmailService.java
@@ -1,7 +1,9 @@
 package bluebottle.send_email.service;
 
 import bluebottle.send_email.model.Email;
+import com.wildbit.java.postmark.client.exception.PostmarkException;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface EmailService {
@@ -9,5 +11,7 @@ public interface EmailService {
     List<Email> findAll();
     Boolean remove(Long id);
     Email save(Email email);
-//    String sendEmail(Email email);
+    String sendEmail(Email email);
+    void senUseSDK(Email email) throws IOException, PostmarkException;
+
 }
--- src/main/java/bluebottle/send_email/service/EmailServiceImpl.java
+++ src/main/java/bluebottle/send_email/service/EmailServiceImpl.java
@@ -2,6 +2,11 @@ package bluebottle.send_email.service;
 
 import bluebottle.send_email.model.Email;
 import bluebottle.send_email.repository.EmailRepository;
+import com.wildbit.java.postmark.Postmark;
+import com.wildbit.java.postmark.client.ApiClient;
+import com.wildbit.java.postmark.client.data.model.message.Message;
+import com.wildbit.java.postmark.client.data.model.message.MessageResponse;
+import com.wildbit.java.postmark.client.exception.PostmarkException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -9,6 +14,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -36,22 +42,33 @@ public class EmailServiceImpl implements EmailService{
         return emailRepository.save(email);
     }
 
-//    @Autowired
-//    private RestTemplate restTemplate;
-//
-//    private final String fromEmail = "hieu.truong@fruitful.io";
-//    private final String token = "ea5851c8-5682-45ac-886d-389594821847";
-//    private final String apiLink = "https://api.postmarkapp.com/email";
-//    @Override
-//    public String sendEmail(Email email) {
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.add("Accept", "application/json");
-//        headers.add("Content-Type", "application/json");
-//        headers.add("X-Postmark-Server-Token", token);
-//        email.setFromMail(fromEmail);
-//        HttpEntity<Email> entity = new HttpEntity<>(email, headers);
-//        return restTemplate.exchange(apiLink, HttpMethod.POST, entity, String.class).getBody();
-//    }
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private final String fromEmail = "hieu.truong@fruitful.io";
+    private final String token = "ea5851c8-5682-45ac-886d-389594821847";
+    private final String apiLink = "https://api.postmarkapp.com/email";
+    @Override
+    public String sendEmail(Email email) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept", "application/json");
+        headers.add("Content-Type", "application/json");
+        headers.add("X-Postmark-Server-Token", token);
+        email.setFromMail(fromEmail);
+        HttpEntity<Email> entity = new HttpEntity<>(email, headers);
+        return restTemplate.exchange(apiLink, HttpMethod.POST, entity, String.class).getBody();
+    }
+
+    @Override
+    public void senUseSDK(Email email) throws IOException, PostmarkException {
+        ApiClient client = Postmark.getApiClient(token);
+        Message message = new Message();
+        message.setFrom(fromEmail);
+        message.setSubject(email.getSubject());
+        message.setTo(email.getToMail());
+        message.setHtmlBody(email.getHtmlBody());
+        MessageResponse messageResponse = client.deliverMessage(message);
+    }
 
 
 }
